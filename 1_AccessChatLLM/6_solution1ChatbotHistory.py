1b170ae634fa4e7246e8037069aee1fbf073111c
AhmedShahan <shahan.ahmed001@gmail.com>
2025-06-05 21:25:46 +0600

    problem in chathistory list append

--- 1_AccessChatLLM/6_solution1ChatbotHistory.py
+++ 1_AccessChatLLM/6_solution1ChatbotHistory.py
@@ -15,8 +15,25 @@ while True:
     if prompt.lower() == "exit":
         break
     response_gemini= modelGemini.invoke(ChatHistory)
-    ChatHistory.append(response_gemini)
+    ChatHistory.append(response_gemini.content)
     
     print("\nGemini Response:",end=":")
     print(response_gemini.content)
     print("*"*50)
+
+print(ChatHistory)
+
+'''
+PROBLEM IN THIS CHAT HISTORY
+
+['what is the largest of 10,22,21,11', 
+'The largest number among 10, 22, 21, and 11 is $\\boxed{22}$.', 
+'multiply with 10 with largest number', 
+'The largest number is 22.  22 multiplied by 10 is 220.', 
+'now substract the result from lowest number', 
+'The lowest number is 10.  Subtracting 220 from 10 gives 10 - 220 = -210', 
+'exit']
+
+
+এখানে কোন চেট কীসের সেটা বুঝা জায় না। মানে কোনটা user এর prompt আর কোনটা AI এর response সেটা বুঝা জায় না। 
+'''
